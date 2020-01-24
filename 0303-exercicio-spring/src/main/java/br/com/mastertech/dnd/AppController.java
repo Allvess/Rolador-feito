c16477992a554861774eb202f5363430a3847d9e
Allvess <patrickalves930@gmail.com>
2020-01-24 16:35:25 -0300

    Criar rotação de dados

---
+++ 0303-exercicio-spring/src/main/java/br/com/mastertech/dnd/AppController.java
@@ -0,0 +1,34 @@
+package br.com.mastertech.dnd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping ("/form")
+    public String form(){
+        return "form";
+    }
+    @PostMapping ("/repost")
+    public  String result(@ModelAttribute Formulario formulario, Model model) throws DadoInvalidoException {
+        try {
+            Dado dado = new Dado(formulario.getLados());
+            Sorteador sorteador = new Sorteador(dado);
+            Resultado resultado = sorteador.sortear(formulario.getVezes());
+
+            model.addAttribute("resultado", resultado);
+            return "repost";
+            }catch (DadoInvalidoException e ) {
+                return "erro";
+        }
+    }
+}
+
